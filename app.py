7f8b8dc9811271a19ceb24c45e13a1c6ad230bc1
payton <payton@paytontaylor.net>
2022-12-09 07:51:52 -0600

    refactored post request into a get request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
@@ -9,9 +9,16 @@ boggle_game = Boggle()
 app.config['SECRET_KEY'] = 'secret'
 app.config['TB_DEBUG_INTERPECT_REDIRECTS'] = False
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def show_game():
     board = boggle_game.make_board()
     session['board'] = board
 
     return render_template('boggle.html',board=board)
+
+@app.route('/check-word')
+def check_word():
+    board = session['board']
+    word = request.args['guess']
+    res = boggle_game.check_valid_word(board,word)
+    return jsonify(result=res)
--- static/boggle.js
+++ static/boggle.js
@@ -1,6 +1,24 @@
-let $guess = $("#guess").val();
+let $form = $('form')
+let $response = $('#response')
+let $showScore = $('#showScore')
+let score = 0
+let timer;
+let seconds = 60;
 
-$guess.click(function (evt) {
-    evt.preventDefault();
-    const res = axios.get('')
-});
+$form.submit(async function (evt) {
+    evt.preventDefault()
+    $guess = $('#guess').val()
+    let res = await axios.get('/check-word', { params: { 'guess': $guess } })
+    let response = res.data.result
+    if (response === 'ok') {
+        $response.text('ok')
+        score += $guess.length
+        $showScore.text(score)
+    }
+    if (response === 'not-on-board') {
+        $response.text('not-on-board')
+    }
+    if (response === 'not-word') {
+        $response.text('not-word')
+    }
+})
